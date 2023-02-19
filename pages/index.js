8ffa4ee71672abaf0a5b64e00bbe4617ca16496c
José Geraldo <jggauss@gmail.com>
2023-02-19 17:35:09 -0300

    Primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// import Link from '../src/componentes/Link';
+// import Footer from '../src/componentes/patterns/Footer';
+
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color:red;
+                    
+//                 }
+                
+//             `}</style>
+//         </>
+//     )
+// }
+
+
+// export default function HomePage() {
+//     return (
+//         <div>
+           
+//                 <Title as="h1">Alura Cases - Home</Title>
+//                 <Link href="/faq" color="green">
+//                     Ir para a página do FAQ
+//                 </Link>
+//                 <Footer/>
+//         </div>
+//     )
+// }
