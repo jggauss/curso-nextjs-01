fcad2d72b4918f5800e8f4b4a2a262d0da2b0d3f
José Geraldo <jggauss@gmail.com>
2023-02-19 17:50:00 -0300

    Adicionando, formatando e tirando aviso de Cross origin do faq commit

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,10 @@
-import Head from 'next/head'
-import { useEffect, useState } from 'react';
-import  Link  from '../src/components/Link';
+// import Head from 'next/head'
+// import { useEffect, useState } from 'react';
+// import  Link  from '../src/components/Link';
 import PageTitle from '../src/components/PageTitle';
+import FAQScreen from '../src/screens/FAQScreens/index.js';
+
+export default FAQScreen;
 
 export async function getStaticProps() {
     const FAQ_URL_API ='https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
@@ -20,32 +23,32 @@ export async function getStaticProps() {
 
 
 
-export default function FAQpage({faq}){
-    console.log("rodando no servidor ou na página")
-    // const [faq,setFaq]=useState([])
-    // useEffect(()=>{
+// export default function FAQpage({faq}){
+//     console.log("rodando no servidor ou na página")
+//     // const [faq,setFaq]=useState([])
+//     // useEffect(()=>{
       
-    // },[])
-    return(
-        <div>
-            <Head>
-            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
-            </Head>
-            <h1>Alura Cases - Página de perguntas FAQ</h1>
-            <Link href="/">
-            Ir para a Home
-            <ul>
-                {faq.map(({answer,question})=>(
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
+//     // },[])
+//     return(
+//         <div>
+//             <Head>
+//             <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
+//             </Head>
+//             <h1>Alura Cases - Página de perguntas FAQ</h1>
+//             <Link href="/">
+//             Ir para a Home
+//             <ul>
+//                 {faq.map(({answer,question})=>(
+//                     <li key={question}>
+//                         <article>
+//                             <h2>{question}</h2>
+//                             <p>{answer}</p>
+//                         </article>
+//                     </li>
+//                 ))}
+//             </ul>
 
-            </Link>
-        </div>
-    )
-}
+//             </Link>
+//         </div>
+//     )
+// }
